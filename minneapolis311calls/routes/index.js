4173113e381cb4b5109af5a2241b53222aec9f6f
Matt Thrasher <matthew.j.thrasher@gmail.com>
2016-09-11 18:59:48 -0500

    Add about and contact views

--- minneapolis311calls/routes/index.js
+++ minneapolis311calls/routes/index.js
@@ -4,9 +4,16 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
+});
 
+/* GET about page. */
+router.get('/about', function (req, res) {
+    res.render('about', { title: 'About' });
+});
 
-
+/* GET contact page. */
+router.get('/contact', function (req, res) {
+    res.render('contact', { title: 'Contact' });
 });
 
 module.exports = router;
