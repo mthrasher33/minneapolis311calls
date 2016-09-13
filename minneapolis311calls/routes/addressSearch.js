e76696c1d70aba1ceb74c680039ca48da0ad2178
Matt Thrasher <matthew.j.thrasher@gmail.com>
2016-09-13 12:27:44 -0500

    Call stored procedure to show 311 calls at address

--- minneapolis311calls/routes/addressSearch.js
+++ minneapolis311calls/routes/addressSearch.js
@@ -1,9 +1,31 @@
 ﻿var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+
+//database connection. where is the correct place for this?
+var connection = mysql.createConnection({
+    host: 'bowie2.c44css47zkdo.us-west-2.rds.amazonaws.com',
+    user: 'master',
+    password: 'fri$nDgramming',
+    database: 'address_data'
+});
+
 
 //Get Address Search Results, curently just searching by DB id for simplicity
 router.get('/:addressId', function (req, res) {
-    res.render('addressSearch', { addressId: req.params.addressId });
+        connection.query("Call rental311callsByAddress(?)", [req.params.addressId], function (err, rows, fields) {
+        if (!err) {
+            console.log('Starting query')
+            res.render('addressSearch', { addressID: req.params.addressId, results: rows[0], title: 'Address Search' });
+
+            //  Send data to the debugger
+            console.log('The solution is: ', rows);
+        }
+        else
+            console.log('Error while performing Query: ' + err);
+    });
+
 });
 
 module.exports = router;
