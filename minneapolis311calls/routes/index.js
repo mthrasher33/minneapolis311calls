dffbe415ffa9dd96b519605fca91e0ca9eef66a5
mikemommsen <mikemommsen@gmail.com>
2016-09-18 06:26:39 -0700

    files to get the map inlace
    we have all of the leaflet.js stuff. this includes imagery, a css file,
    and js file all downloaded from
    [here](http://cdn.leafletjs.com/leaflet/v1.0.0-rc.3/leaflet.zip).
    
    i modified index.js to have the logic server side for parsing queries
    for point data and returning geojson, and i created a new view called
    map.jade

--- minneapolis311calls/routes/index.js
+++ minneapolis311calls/routes/index.js
@@ -1,7 +1,9 @@
-﻿var express = require('express');
+
+
+var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
-
+var geojson = require('geojson')
 
 //database connection. where is the correct place for this?
 var connection = mysql.createConnection({
@@ -42,5 +44,40 @@ router.get('/about', function (req, res) {
 router.get('/contact', function (req, res) {
     res.render('contact', { title: 'Contact' });
 });
+router.get('/map', function (req, res){
+   res.render('map', {title: "Map"})
+});
+router.get('/map.geojson', function (req, res){
+  connection.query('SELECT distinct round(x, 3) as x, round(y, 3) as y, subjectname from Complaint311 limit 100', function (err, rows, fields) {
+      if (!err) {
+          var geo
+          console.log('', geo)
+          geo = geojson.parse(rows, {Point: ['y', 'x']})
+          res.send(geo)
+      }
+      else
+          console.log('Error while performing Query: ' + err);
+  });
+});
+router.get('/map/geojson', function (req, res){
+  var q = '\
+      SELECT distinct round(x(geom), 6) as x, round(y(geom), 6) as y, \
+      subjectname from Complaint311 \
+      where st_contains(st_Envelope(geomfromtext(\'LineString(? ?, ? ?)\', 4326)), geom)\
+      limit 100\
+      '
+  console.log(q)
+  var params = [+req.query.East,+req.query.South, +req.query.West, +req.query.North]
+  console.log(params)
+  connection.query(q, params, function (err, rows, fields) {
+      if (!err) {
+          geo = geojson.parse(rows, {Point: ['y', 'x']})
+          res.send(geo)
+      }
+      else
+          console.log('Error while performing Query: ' + err);
+  });
+});
+
 
 module.exports = router;
