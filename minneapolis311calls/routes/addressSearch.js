7be74a7dca3cea350212560b2a00a0ccab1269f2
Matthew Thrasher <matthewth@marvin.com>
2016-10-21 14:48:36 -0500

    Add maps to address page

--- minneapolis311calls/routes/addressSearch.js
+++ minneapolis311calls/routes/addressSearch.js
@@ -13,11 +13,14 @@ router.get('/:address', function (req, res) {
     datalayer.rental311callsByAddress(req.params.address, function (err, rows, fields) {
         if (!err) {
             console.log('Starting query')
+            //console.log(rows);
             if (rows.length) { //if we have results
-                res.render('addressSearch', { address: req.params.address, results: rows[0], title: 'Address Search' });
+                res.render('addressSearch', { address: req.params.address, results: rows, title: 'Address Search' });
             } else { //we have no results
-                res.render('addressSearch', { address: req.params.address, title: 'Address Search' });
+//                res.render('addressSearch', { address: req.params.address, title: 'Address Search' });
             };
+                    console.log("ROWS[0]: " + rows[0]);
+                    console.log("LENGTH: " + rows[0].length);
         }
         else
             console.log('Error while performing Query: ' + err);
