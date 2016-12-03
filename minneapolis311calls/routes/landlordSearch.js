f1f8afc5e4af6bd5c0e742e26682ef560d0d6828
AOjen <adam.l.oien@gmail.com>
2016-12-03 12:10:24 -0600

    New design applied to the LandlordSearch page
    Applied the new design to the LandlordSearch page.  Added the Properties
    Owned data.

--- minneapolis311calls/routes/landlordSearch.js
+++ minneapolis311calls/routes/landlordSearch.js
@@ -8,9 +8,16 @@ router.get('/:landlordName', function (req, res) {
 
         datalayer.rental311callsByLandlord(req.params.landlordName, function (err, rows, fields) {
             if (!err) {     
-                var firstResult = rows[0];
-                res.render('landlordSearch', { landlordName: req.params.landlordName, resultsLl: rows[0], title: 'Landlord Search' });
-
+                var propertyCountForOwner = null;
+                datalayer.PropertiesOwnedByLandlord(rows[0][0].APP_NAME, function (err, properties, fields) {
+                    if (!err) {
+                        var firstResult = rows[0];
+                        propertyCountForOwner = properties[0].length;
+                        res.render('landlordSearch', { landlordName: req.params.landlordName, resultsLl: rows[0], propertyCountForOwner: propertyCountForOwner, title: 'Landlord Search' });
+                    }
+                    else
+                        console.log('Error while performing Query: ' + err);
+                });
             //  Send data to the debugger
             console.log('The solution is: ', rows);
         }
